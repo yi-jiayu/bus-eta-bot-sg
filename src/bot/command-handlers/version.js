c0c7c137c5f662a7742d6afe29b7c3dc05c54400
Jiayu <yi-jiayu@users.noreply.github.com>
2016-10-21 17:59:42 +0800

    Added analytics code to /version handler

--- src/bot/command-handlers/version.js
+++ src/bot/command-handlers/version.js
@@ -1,5 +1,5 @@
 "use strict";
-
+const debug = require('debug')('bus-eta-bot-sg:bot/command-handlers/about');
 const telegram = require('../../telegram');
 
 /**
@@ -8,8 +8,18 @@ const telegram = require('../../telegram');
  * @param {IncomingTextMessage} msg
  */
 module.exports = function (bot, msg) {
+  const action = 'version';
+
   const text = `Bus Eta Bot \`v${process.env.BOT_VERSION}\``;
   const config = {parse_mode: 'Markdown'};
 
-  return new telegram.OutgoingTextMessage(text, config).send(msg.chat_id);
+  return new telegram.OutgoingTextMessage(text, config).send(msg.chat_id)
+
+  // analytics
+    .then(() => Promise.all([bot.analytics.logAction(action), bot.analytics.logUser(action, msg)]))
+
+    .catch(err => {
+      debug(err);
+      return bot.analytics.logError(action, err.message);
+    });
 };
